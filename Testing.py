e459d37d1eb88216743dd35af91ef21e26ffac0d
Sahel Abraham <abrahamjsahel@gmail.com>
2025-03-16 02:49:32 -0500

    Added more functionality to csv integration

--- Testing.py
+++ Testing.py
@@ -5,5 +5,12 @@ from Layer import Layer
 
 with open('Network1.csv', 'r') as file:
     csvreader = csv.reader(file)
-    if next(csvreader) == "Layer":
-        layer = Layer()
+    layer = Layer(np.array(next(csvreader)), np.array(next(csvreader)))
+    weight_row = [next(csvreader)]
+    weights = np.array(weight_row)
+    while(weight_row != [['End']]):
+        weights = np.append(weights, weight_row, axis=0)
+        weight_row = [next(csvreader)]
+    print(weights)
+    print(layer)
+    
